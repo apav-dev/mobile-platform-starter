820b96f1d248d95a63e2367550b639ee31b2bdbe
apav-dev <apavlick@yext.com>
2023-09-17 11:11:50 -0400

    image carousel ui fixes

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,7 +1,6 @@
 import { GalleryImage } from "../types/yext";
 import { useState } from "react";
 import { motion, AnimatePresence, MotionConfig } from "framer-motion";
-import * as React from "react";
 import { LeftChevronIcon } from "./icons/LeftChevronIcon";
 import { RightChevronIcon } from "./icons/RightChevronIcon";
 import { v4 as uuidv4 } from "uuid";
@@ -10,54 +9,54 @@ export interface ImageCarouselProps {
   images: GalleryImage[];
 }
 
-// TODO: Handle images that don't take full width
-// TODO: Figure out why I can't use Yext Image component
-// TODO: Address larger breakpoints
 export const ImageCarousel = ({ images }: ImageCarouselProps) => {
   let [index, setIndex] = useState(0);
 
+  console.log(images);
+
   return (
-    <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-      <div className="relative overflow-hidden z-0">
-        <motion.div className="flex" animate={{ x: `-${index * 100}%` }}>
-          {images.slice(1).map((image, i) => (
-            // <Image image={image} layout="fixed" width={310} height={180} className="mx-auto" />
-            <img
-              key={`image-${uuidv4()}`}
-              src={image.image.url}
-              className="object-cover aspect-3/2 max-h-80"
-            />
-          ))}
-        </motion.div>
-        <AnimatePresence initial={false}>
-          {index > 0 && (
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 0.7 }}
-              exit={{ opacity: 0, pointerEvents: "none" }}
-              whileHover={{ opacity: 1 }}
-              className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-              onClick={() => setIndex(index - 1)}
-            >
-              <LeftChevronIcon />
-            </motion.button>
-          )}
-        </AnimatePresence>
-        <AnimatePresence initial={false}>
-          {index + 1 < images.length && (
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 0.7 }}
-              exit={{ opacity: 0, pointerEvents: "none" }}
-              whileHover={{ opacity: 1 }}
-              className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-              onClick={() => setIndex(index + 1)}
-            >
-              <RightChevronIcon />
-            </motion.button>
-          )}
-        </AnimatePresence>
-      </div>
-    </MotionConfig>
+    <div className="flex justify-center">
+      <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
+        <div className="relative overflow-hidden z-0 w-full max-w-sm flex">
+          <motion.div className="flex" animate={{ x: `-${index * 100}%` }}>
+            {images.map((image, i) => (
+              <img
+                key={`image-${uuidv4()}`}
+                src={image.image.url}
+                className="object-cover aspect-3/2 max-h-80"
+              />
+            ))}
+          </motion.div>
+          <AnimatePresence initial={false}>
+            {index > 0 && (
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.7 }}
+                exit={{ opacity: 0, pointerEvents: "none" }}
+                whileHover={{ opacity: 1 }}
+                className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                onClick={() => setIndex(index - 1)}
+              >
+                <LeftChevronIcon />
+              </motion.button>
+            )}
+          </AnimatePresence>
+          <AnimatePresence initial={false}>
+            {index + 1 < images.length && (
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.7 }}
+                exit={{ opacity: 0, pointerEvents: "none" }}
+                whileHover={{ opacity: 1 }}
+                className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                onClick={() => setIndex(index + 1)}
+              >
+                <RightChevronIcon />
+              </motion.button>
+            )}
+          </AnimatePresence>
+        </div>
+      </MotionConfig>
+    </div>
   );
 };
